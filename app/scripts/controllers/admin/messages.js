32b7350e453479fadb84e4f3df44452a881bfc38
Mule52 <bamueller@gmail.com>
2014-07-02 08:03:58 -0600

    updating

---
+++ app/scripts/controllers/admin/messages.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('yeomanMeanApp')
+  .controller('AdminMessagesCtrl', ['$scope', '$location', '$timeout', 'ContactMessageService', 
+    function ($scope, $location, $timeout, ContactMessageService) {
+
+    //Init
+    (function () {
+
+    }());
+
+  }]);
+
